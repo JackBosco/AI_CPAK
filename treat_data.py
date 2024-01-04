dc3f7feba321249641a58a56493c1a150604faa0
JackBosco <boscoj24@mail.wlu.edu>
2024-01-04 15:01:36 -0500

    fixing the morphology algorithm

--- treat_data.py
+++ treat_data.py
@@ -11,24 +11,24 @@ import sys
 df = pd.read_excel("raw/mako_data.xlsx", header=1)
 col_headers = list(df.columns)
 
+# TODO change name of the femoral and tibial rotation for the last edited screen
+
 df.rename(columns={"HAS SUMMARY IMAGE (YES=1,NO=0)": "has_summary_image"}, inplace=True)
-# df.rename(columns={"HAS LAST EDITED PLAN IMAGE (YES=1,NO=0)": "has_last_image"}, inplace=True)
 
 summary = df[df["has_summary_image"] == 1].copy().set_index(df.columns[0])
-# last_only = df[(df["has_last_image"] == 1 ) & (df["has_summary_image"] == 0)]
 
-planned_headers = {'jlo': 'Femoral Rotation: Coronal (Varus = +, Valgus = -) (degrees)', 
+planned_headers = {'jlo': 'Femoral Rotation: Coronal (Varus = +, Valgus = -) (degrees)', #not correct
 				   'hka' : 'Tibial Rotation: Coronal (Varus = +, Valgus = -) (degrees)'
 				}
-preop_headers = {'hka': 'Joint Line: aHKA (Varus = +, Valgus = -) (degrees)', 
-				 'jlo': 'Joint Line: JLO (degrees)'
+preop_headers = {'hka': 'Joint Line: aHKA (Varus = +, Valgus = -) (degrees)', # mult by -1
+				 'jlo': 'Joint Line: JLO (degrees)' # subtract 180
 				}
 
 summary.dropna(axis='index', subset=list(preop_headers.values()) + list(planned_headers.values()), inplace=True) # drops rows without planned and postop hka, jlo
 
 out = summary.loc[:, list(planned_headers.values()) + list(preop_headers.values())]
 
-def get_morphology(jlo, hka, jlo_thresh=2, hka_thresh=1):
+def get_morphology(jlo, hka, jlo_thresh=3, hka_thresh=2): #
 	morph_table = [	[1, 2, 3],
 					[4, 5, 6],
 					[7, 8, 9]]
@@ -50,7 +50,7 @@ def get_morphology(jlo, hka, jlo_thresh=2, hka_thresh=1):
 	return morph_table[jlo_num][hka_num]
 
 planned_morphs = out.apply(lambda x: get_morphology(x[planned_headers['jlo']], x[planned_headers['hka']]), axis=1)
-preop_morphs = out.apply(lambda x: get_morphology(x[preop_headers['jlo']]-180, x[preop_headers['hka']]), axis=1)
+preop_morphs = out.apply(lambda x: get_morphology(x[preop_headers['jlo']]-180, x[preop_headers['hka']]*-1), axis=1)
 
 out["Planned Morphology"] = planned_morphs
 out["Pre-op Morphology"] = preop_morphs
