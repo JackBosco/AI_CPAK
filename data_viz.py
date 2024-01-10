c1357db3f059525b9bc32d8dd9c5f9f405fee394
Jack Bosco <boscoj24@mail.wlu.edu>
2024-01-10 17:31:53 -0500

    avg data in vis

--- data_viz.py
+++ data_viz.py
@@ -11,33 +11,65 @@ preop_hka, preop_jlo = df.loc[:, cols[0]], df.loc[:, cols[1]]
 postop_hka, postop_jlo = df.loc[:, cols[2]], df.loc[:, cols[3]]
 
 # ==========plot preop data================
-fig, ax = plt.subplots(figsize = (8,8))
+fig, ax = plt.subplots(nrows=2, figsize = (8,8), sharex='all', sharey='all')
 
 #plotting black for preop
-ax.scatter(x=preop_hka, 
+ax[0].scatter(x=preop_hka, 
 		   y=preop_jlo,
 			c='black', label='pre-op',) 
 
 #plotting blue for postop
-ax.scatter(x=postop_hka,
+ax[0].scatter(x=postop_hka,
 		   y=postop_jlo,
 		   c='blue', label='post-op')
 
-ax.invert_yaxis()
 
-max_x = max(max(preop_hka), max(postop_hka))
-min_x = min(min(preop_hka), min(postop_hka))
+preop_verus = df.loc[df[cols[5]] % 3 == 1, cols[0]:cols[1]]
+avg_preop_verus = np.average(preop_verus[cols[0]]), np.average(preop_verus[cols[1]])
+
+postop_verus = df.loc[df[cols[4]] % 3 == 1, cols[2]:cols[3]]
+avg_postop_verus = np.average(postop_verus[cols[2]]), np.average(postop_verus[cols[3]])
+
+postop_aligned = df.loc[df[cols[4]] % 3 == 2, cols[2]:cols[3]]
+avg_postop_aligned = np.average(postop_aligned[cols[2]]), np.average(postop_aligned[cols[3]])
+
+preop_aligned = df.loc[df[cols[5]] % 3 == 2, cols[0]:cols[1]]
+avg_preop_aligned = np.average(preop_aligned[cols[0]]), np.average(preop_aligned[cols[1]])
+
+preop_valgus = df.loc[df[cols[5]] % 3 == 0, cols[0]:cols[1]]
+avg_preop_valgus = np.average(preop_valgus[cols[0]]), np.average(preop_valgus[cols[1]])
+
+postop_valgus = df.loc[df[cols[4]] % 3 == 0, cols[2]:cols[3]]
+avg_postop_valgus = np.average(postop_valgus[cols[2]]), np.average(postop_valgus[cols[3]])
+
+
+ax[1].scatter(avg_postop_verus[0], avg_postop_verus[1], c='purple')
+ax[1].annotate('verus postop', avg_postop_verus)
+ax[1].scatter(avg_preop_verus[0], avg_preop_verus[1], c='purple')
+ax[1].annotate('verus preop', avg_preop_verus)
+ax[1].scatter(avg_preop_aligned[0], avg_preop_aligned[1], c='brown')
+ax[1].annotate('aligned preop', avg_preop_aligned)
+ax[1].scatter(avg_postop_aligned[0], avg_postop_aligned[1], c='brown')
+ax[1].annotate('aligned postop', avg_postop_aligned)
+ax[1].scatter(avg_preop_valgus[0], avg_preop_valgus[1], c='green')
+ax[1].annotate('valgus preop', avg_preop_valgus)
+ax[1].scatter(avg_postop_valgus[0], avg_postop_valgus[1], c='green')
+ax[1].annotate('valgus postop', avg_postop_valgus)
 
 #plot the lines
-plt.axhline(y=177)
-plt.axhline(y=183)
-plt.axvline(x=-2)
-plt.axvline(x=2)
+ax[0].invert_yaxis()
+for a in ax:
+	a.axhline(y=177)
+	a.axhline(y=183)
+	a.axvline(x=-2)
+	a.axvline(x=2)
+	a.set_xlabel("aHKA (Varus < -2º, Valgus > 2º)")
+	a.set_ylabel("JLO (Apex Distal > 183º, Apex Proximal < 177º)")
 
 #axis titles
-ax.set_title('Pre and Post op Knee Alignment Morphologies')
-ax.set_xlabel("aHKA (Varus < -2º, Valgus > 2º)")
-ax.set_ylabel("JLO (Apex Distal > 183º, Apex Proximal < 177º)")
+ax[0].set_title('Pre and Post op Knee Alignment Morphologies')
+ax[1].set_title('Average Pre and Post op Knee Alignment Morphologies')
 
-plt.legend()
+ax[0].legend()
+ax[1].legend(loc='lower right')
 plt.show()
