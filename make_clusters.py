8516f360ba76c4086bdeb15f49fa8477dbb77411
Jack Bosco <boscoj24@mail.wlu.edu>
2024-01-21 12:58:42 -0500

    new options age, bmi for clustering

--- make_clusters.py
+++ make_clusters.py
@@ -6,6 +6,7 @@ from click import group
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import sys
 
 try:
 	f = open('treated/morphologies.csv', 'r')
@@ -13,7 +14,18 @@ try:
 except:
 	raise Exception("morphologies.csv not found in ./treated directory. This file is not part of the standard repo. See ./treated/README.md for more details")
 df = pd.read_csv('treated/morphologies.csv')
+df1 = pd.read_excel('raw/mako_data.xlsx', header=1)
 data = df.iloc[:, 1:5]
+
+# optional arguments for including more elements in the clustering
+options = []
+if 'age' in sys.argv:
+	options.append('age')
+	data['age'] = df1.loc[:, 'Age at Surgery'] / 100
+if 'bmi' in sys.argv:
+	options.append('bmi')
+	data['bmi'] = df1.loc[:, 'BMI'] / 50
+
 N_CLUSTERS = 3
 colors = ['black', 'blue', 'red', 'green']
 
@@ -25,6 +37,7 @@ data['cluster'] = clusters
 
 # make data visualization with the clusters
 fig, ax = plt.subplots(nrows=2, figsize = (8,8), sharex='col', sharey='all')
+	
 
 #plotting black for preop
 for i, color in zip(range(N_CLUSTERS), colors):
@@ -48,5 +61,6 @@ for a in ax:
 
 ax[0].set_title("Pre-Op Alignment")
 ax[1].set_title("Post-Op Alignment")
-fig.suptitle("Clusters of Pre-Op and Post-Op Morphologies with KMeans n_clusters=3")
+fig.suptitle("Clusters of Pre-Op and Post-Op Morphologies with KMeans\n"+
+			 "[n_clusters=3 " + ' '.join(options) + ']')
 plt.show()
