FIXED BAD VISUALIZATION using postop for the averages

data_viz.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from matplotlib import legend
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -30,37 +31,37 @@ ax[0].scatter(x=postop_hka,
 		   c='blue', label='post-op')
 
 
-preop_verus = df.loc[df[cols[5]] % 3 == 1, cols[0]:cols[1]]
+preop_verus = df.loc[df["Pre-op Morphology"] % 3 == 1, cols[0]:cols[1]]
 avg_preop_verus = np.average(preop_verus[cols[0]]), np.average(preop_verus[cols[1]])
 
-postop_verus = df.loc[df[cols[4]] % 3 == 1, cols[2]:cols[3]]
+postop_verus = df.loc[df["Pre-op Morphology"] % 3 == 1, cols[2]:cols[3]]
 avg_postop_verus = np.average(postop_verus[cols[2]]), np.average(postop_verus[cols[3]])
 
-postop_aligned = df.loc[df[cols[4]] % 3 == 2, cols[2]:cols[3]]
+postop_aligned = df.loc[df["Pre-op Morphology"] % 3 == 2, cols[2]:cols[3]]
 avg_postop_aligned = np.average(postop_aligned[cols[2]]), np.average(postop_aligned[cols[3]])
 
-preop_aligned = df.loc[df[cols[5]] % 3 == 2, cols[0]:cols[1]]
+preop_aligned = df.loc[df["Pre-op Morphology"] % 3 == 2, cols[0]:cols[1]]
 avg_preop_aligned = np.average(preop_aligned[cols[0]]), np.average(preop_aligned[cols[1]])
 
-preop_valgus = df.loc[df[cols[5]] % 3 == 0, cols[0]:cols[1]]
+preop_valgus = df.loc[df["Pre-op Morphology"] % 3 == 0, cols[0]:cols[1]]
 avg_preop_valgus = np.average(preop_valgus[cols[0]]), np.average(preop_valgus[cols[1]])
 
-postop_valgus = df.loc[df[cols[4]] % 3 == 0, cols[2]:cols[3]]
+postop_valgus = df.loc[df["Pre-op Morphology"] % 3 == 0, cols[2]:cols[3]]
 avg_postop_valgus = np.average(postop_valgus[cols[2]]), np.average(postop_valgus[cols[3]])
 
 
-ax[1].scatter(avg_postop_verus[0], avg_postop_verus[1], c='purple')
-ax[1].annotate('varus postop', avg_postop_verus)
+ax[1].scatter(avg_postop_verus[0], avg_postop_verus[1], c='purple', label='Average for verus preop group')
+ax[1].annotate('Postop', avg_postop_verus)
 ax[1].scatter(avg_preop_verus[0], avg_preop_verus[1], c='purple')
-ax[1].annotate('varus preop', avg_preop_verus)
-ax[1].scatter(avg_preop_aligned[0], avg_preop_aligned[1], c='brown')
-ax[1].annotate('aligned preop', avg_preop_aligned)
+ax[1].annotate('Preop', avg_preop_verus)
+ax[1].scatter(avg_preop_aligned[0], avg_preop_aligned[1], c='brown', label='Average for aligned preop group')
+ax[1].annotate('Preop', avg_preop_aligned)
 ax[1].scatter(avg_postop_aligned[0], avg_postop_aligned[1], c='brown')
-ax[1].annotate('aligned postop', avg_postop_aligned)
-ax[1].scatter(avg_preop_valgus[0], avg_preop_valgus[1], c='green')
-ax[1].annotate('valgus preop', avg_preop_valgus)
+ax[1].annotate('Postop', avg_postop_aligned)
+ax[1].scatter(avg_preop_valgus[0], avg_preop_valgus[1], c='green', label='Average for valgus preop group')
+ax[1].annotate('Preop', avg_preop_valgus)
 ax[1].scatter(avg_postop_valgus[0], avg_postop_valgus[1], c='green')
-ax[1].annotate('valgus postop', avg_postop_valgus)
+ax[1].annotate('Postop', avg_postop_valgus)
 
 #plot the lines
 ax[0].invert_yaxis()
@@ -77,5 +78,6 @@ ax[0].set_title('Pre and Post op Knee Alignment Morphologies')
 ax[1].set_title('Average Pre and Post op Knee Alignment Morphologies')
 
 ax[0].legend()
+ax[1].legend()
 #ax[1].legend(loc='lower right')
 plt.show()