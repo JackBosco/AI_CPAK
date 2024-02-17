88beb485ab90addcb7a76d6ebcd4a84ee927fd47
JackBosco <boscoj24@mail.wlu.edu>
2024-02-17 15:07:51 -0500

    good regressions

--- regression.py
+++ regression.py
@@ -2,6 +2,7 @@
 	Regression analysis for the alignment data using linear regression
 	Author: Jack Bosco
 """
+from curses import beep
 from sklearn.svm import NuSVR
 import config
 import pandas as pd
@@ -12,16 +13,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 import numpy as np
 
 # read in the data, split into X and y
 df = pd.read_csv(config.treated_path, index_col=0)
-X = df.iloc[:, [0, 1, 6, 7, 8]] # preop hka, preop jlo, age, bmi, femoral transverse rotation
+X = df.iloc[:, [0]] #1, 6, 7, 8]] # preop hka, preop jlo, age, bmi, femoral transverse rotation
 y = df.iloc[:, 2] # planned hka
 
 # split into training and testing data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) # 40% training, 10% testing, 42 is the answer to everything
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) # 75% training, 25% testing, 42 is the answer to everything
 
 # standardize the data
 scaler = StandardScaler()
@@ -33,7 +35,7 @@ normalizer = MinMaxScaler(feature_range=(-1, 1))
 X_train_normalized = normalizer.fit_transform(X_train)
 X_test_normalized = normalizer.transform(X_test)
 
-def test_model(fit_model, model_name, testset, trainset):
+def test_model(fit_model, model_name, testset, trainset, three_d=False):
 	y_pred = fit_model.predict(testset[0])
 	error = mean_squared_error(testset[1], y_pred)
 	print(f"Mean Squared Error for {model_name}: {error}")
@@ -42,65 +44,110 @@ def test_model(fit_model, model_name, testset, trainset):
 	print(f"Mean Squared Error for {model_name} on training data: {error_train}")
 	print(f"Score: {fit_model.score(testset[0], testset[1])}")
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(X_test.iloc[:, 0], X_test.iloc[:, 4], y_test, color='blue')
-	#mn, mx = X.iloc[:, 0].min(), X.iloc[:, 0].max()
-	#grad = np.linspace(mn, mx, 200)
-	dt = pd.DataFrame({'0':np.array(X_test.iloc[:, 0]),'.5':np.array(X_test.iloc[:, 4]), '1':pd.Series(y_pred)})
+	y_all = np.concatenate((y_pred, y_pred_train))
+	x_app = np.concatenate((X_test.iloc[:,0], X_train.iloc[:, 0]))
+	dt1 = pd.DataFrame({'0':np.array(x_app), '1':pd.Series(y_all)})
+	dt = pd.DataFrame({'0':np.array(X_test.iloc[:, 0]),'1':pd.Series(y_pred)})
+	dt2 = pd.DataFrame({'0':np.array(X_train.iloc[:, 0]),'1':pd.Series(y_pred_train)})
 	dt.sort_values(by='0', inplace=True)
-	#print(dt)
-	#plt.plot(X_train.iloc[:, 0], pd.Series(y_pred_train), color='red')
-	ax.plot(dt.iloc[:, 0], dt.iloc[:, 1], dt.iloc[:, 2], color='red')
-	#dt1 = pd.DataFrame({'0':np.array(X_test.iloc[:, 4]), '1':pd.Series(y_pred)})
-	#dt1.sort_values(by='0', inplace=True)
-	#plt.plot(dt1.iloc[:, 0], dt1.iloc[:, 1], color='green')
-	plt.title(f'{model_name} Model Prediction for Post-op HKA from Pre-op HKA')
-	plt.xlabel('Pre-op HKA')
-	plt.ylabel('Femoral Coronal Rotation')
-	ax.set_zlabel('Predicted Planned HKA')
+	dt1.sort_values(by='0', inplace=True)
+	dt2.sort_values(by='0', inplace=True)
+
+	# plot the training data
+	plt.scatter(X_train.iloc[:, 0], y_train, color='blue', label='Training Data')
+
+	# plot the line of best fit
+	plt.plot(dt1.loc[:, '0'], dt1.loc[:, '1'], color='red', label='Regression Line of Best Fit')
+	
+	# add axis lines
+	plt.axvline(x=-2)
+	plt.axvline(x=2)
+	plt.axhline(y=-2)
+	plt.axhline(y=2)
+
+	# plot the error
+	yerr = np.abs(dt.loc[:,'0']-dt.loc[:,'1'])
+	plt.fill_between(dt['0'], dt['1']+yerr, dt['1']-yerr, color='red', alpha=.2, label='Error Room')
+	plt.fill_between(dt['0'], (dt['1']+.33*yerr), (dt['1']-.33*yerr), color='red', alpha=.4, label='33% Error Room')
+	
+	plt.title(f'{model_name} Model Prediction for Planned aHKA from Pre-op aHKA')
+	plt.xlabel('Pre-op aHKA')
+	plt.ylabel('Planned aHKA')
+	plt.legend()
 	plt.show()
+	plt.savefig(f'{model_name}'.replace(' ','_') +'_regression.png')
 	plt.close()
+	
 
 
-# linear regression model
-lin = LinearRegression()
-# fit the model with polynomial features
-poly = PolynomialFeatures(degree=2) # parabolas are cool
-X_train_poly = poly.fit_transform(X_train_scalar)
-X_test_poly = poly.transform(X_test_scalar)
-lin.fit(X_train_poly, y_train)
+def do_lin():
+	# linear regression model
+	lin = LinearRegression()
+	# fit the model with polynomial features
+	poly = PolynomialFeatures(degree=3) # sigmoidal or tanh or something
+	X_train_poly = poly.fit_transform(X_train_scalar)
+	X_test_poly = poly.transform(X_test_scalar)
+	lin.fit(X_train_poly, y_train)
 
-test_model(lin, 
-		   "linear regression",
-			testset=(X_test_poly, y_test),
-			trainset=(X_train_poly, y_train))
-
+	test_model(lin, 
+			"linear regression",
+				testset=(X_test_poly, y_test),
+				trainset=(X_train_poly, y_train))
+	
 	
 # feed-forward neural network model
-def do_mlp():
-	mlp = MLPRegressor()
-	clf = GridSearchCV(mlp, {'hidden_layer_sizes': [(5,), (10,), (20,), (30,)],
-							'max_iter': [1000, 2000, 3000],
-							'solver': ['sgd', 'adam']})
-	clf.fit(X_train_normalized, y_train)
-	test_model(clf.best_estimator_, 
+def do_mlp(train=True):
+	if train:
+		mlp = MLPRegressor()
+		clf = GridSearchCV(mlp, {'hidden_layer_sizes': [(5,), (10,), (20,), (30,)],
+								'max_iter': [1000, 2000, 3000],
+								'solver': ['sgd', 'adam'],
+								'activation': ['tanh']})
+		clf.fit(X_train_normalized, y_train)
+		best = clf.best_estimator_
+		pickle.dump(best, open('neural_network.h5', 'wb'))
+	else:
+		best=pickle.load(open('neural_network.h5', 'rb'))
+		
+	test_model(best, 
 			"neural network",
 				testset=(X_test_normalized, y_test),
 				trainset=(X_train_normalized, y_train))
 
-#do_mlp() # this takes a long time to run (R.I.P. YOUR CPU)
 
 # Support Vector Machine model
-parameters = {'kernel':('linear', 'rbf'),
-			  'degree': [2, 3, 4],
-			  'C':[1, .5, .1], 'gamma':[1, .1, .01, .001, .0001]}
+def do_svm(train=True):
+	if train:
+		parameters = {'kernel':('linear', 'rbf'),
+					'degree': [2, 3, 4],
+					'C':[1, .5, .1], 'gamma':[1, .1, .01, .001, .0001]}
+		svr = NuSVR()
+		clf = GridSearchCV(svr, parameters)
+		clf.fit(X_train_scalar, y_train)
+		best = clf.best_estimator_
+		pickle.dump(best, open('support_vector_machine.h5', 'wb'))
+	else:
+		best=pickle.load(open('support_vector_machine.h5', 'rb'))
+	
+	test_model(best,
+			"support vector machine",
+				testset=(X_test_scalar, y_test),
+				trainset=(X_train_scalar, y_train))
 
-svr = NuSVR()
-clf = GridSearchCV(svr, parameters)
-clf.fit(X_train_scalar, y_train)
-# print(cross_validate(clf, scaler.transform(X), y, cv=5))
-test_model(clf.best_estimator_,
-		   "support vector machine",
-			testset=(X_test_scalar, y_test),
-			trainset=(X_train_scalar, y_train))
+# Gaussian Process model
+def do_gaus():
+	from sklearn.gaussian_process import GaussianProcessRegressor
+	from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+	kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+	gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=2)
+	gp.fit(X_train_scalar, y_train)
+	test_model(gp,
+			"gaussian process",
+				testset=(X_test_scalar, y_test),
+				trainset=(X_train_scalar, y_train))
+
+if __name__ == '__main__':
+	do_lin()
+	do_mlp()#train=False) # this takes a long time to run (R.I.P. YOUR CPU)
+	do_svm()#train=False)
+	#do_gaus()
