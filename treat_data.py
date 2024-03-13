b61690eb883663fde43789cc8c12881699084c94
Jack Bosco <boscoj24@mail.wlu.edu>
2024-03-13 19:24:43 -0400

    added ldfa, mpta to output

--- treat_data.py
+++ treat_data.py
@@ -25,18 +25,38 @@ summary = df[df["has_summary_image"] == 1].copy()
 
 # declare headers 
 
-planned_headers = {	'ldfa': 'Femoral Rotation: Coronal (Varus = +, Valgus = -) (degrees)',
-				   	'mpta' : 'Tibial Rotation: Coronal (Varus = +, Valgus = -) (degrees).1',
+planned_headers = {	'ldfa': 'Planned LDFA',
+				   	'mpta' : 'Planned MPTA',
 					'hka' : 'Planned aHKA (Varus < -2º, Valgus > 2º)',
 					'jlo' : 'Planned JLO (Apex Proximal > 183º, Apex Distal < 177º)'
 				}
 preop_headers = {'hka': 'Pre-op aHKA (Varus < -2º, Valgus > 2º)', # mult by -1
-				 'jlo': 'Pre-op JLO (Apex Proximal > 183º, Apex Distal < 177º)' # subtract 180
+				 'jlo': 'Pre-op JLO (Apex Proximal > 183º, Apex Distal < 177º)', # subtract 180
+				 'mpta': 'Pre-op mpta',
+				 'ldfa': 'Pre-op ldfa'
 				}
+pre_mpta_old = 'Joint Line: MPTA (degrees)'
+pre_ldfa_old = 'Joint Line: LDFA (degrees)'
 old_hka = 'Joint Line: aHKA (Varus = +, Valgus = -) (degrees)' 
 old_jlo = 'Joint Line: JLO (degrees)'
 
+post_ldfa_old = 'Femoral Rotation: Coronal (Varus = +, Valgus = -) (degrees)'
+post_mpta_old = 'Tibial Rotation: Coronal (Varus = +, Valgus = -) (degrees).1'
+
+# rename the bad preop hka and jlo headers to include pre-op
 summary.rename(columns={old_hka: preop_headers['hka'], old_jlo: preop_headers['jlo']}, inplace=True)
+
+# rename bad postop mpta and jlo headers
+summary.rename(columns={
+				post_mpta_old: planned_headers['mpta']
+				, post_ldfa_old: planned_headers['ldfa']
+				}, inplace=True)
+
+# rename the bad preop mpta and jlo headers
+summary.rename(columns={
+				pre_mpta_old: preop_headers['mpta']
+				, pre_ldfa_old: preop_headers['ldfa']
+				}, inplace=True)
 
 summary.dropna(axis='index', 
 			   subset=list(preop_headers.values()) +
@@ -44,7 +64,7 @@ summary.dropna(axis='index',
 					inplace=True) # drops rows without planned and postop hka, jlo, ldfa, mpta
 
 #==================== preop ======================
-# hka, jlo for pre-op
+# hka, jlo, mpta, ldfa for pre-op
 out = summary.loc[:, list(preop_headers.values()) + ['Age at Surgery', 'BMI']]
 # hka *= -1
 out[preop_headers['hka']] *= -1
@@ -63,11 +83,20 @@ planned_vals[planned_headers['mpta']] += 90
 # LDFA = summary femoral coronal rotation + 90
 planned_vals[planned_headers['ldfa']] += 90
 
+# add planned ldfa and mpta to output
+out[planned_headers['ldfa']] = summary[planned_headers['ldfa']]
+out[planned_headers['mpta']] = summary[planned_headers['mpta']]
+
 #hka, jlo for post-op
 #aHKA = MPTA - LDFA
 out[planned_headers['hka']] = planned_vals[planned_headers['mpta']] - planned_vals[planned_headers['ldfa']]
 #JLO = MPTA + LDFA
 out[planned_headers['jlo']] = planned_vals[planned_headers['mpta']] + planned_vals[planned_headers['ldfa']]
+
+# =================== move ldfa and mpta to the back =============
+for label in [planned_headers['ldfa'], planned_headers['mpta'], preop_headers['ldfa'], preop_headers['mpta']]:
+	out[label] = out.pop(label) 
+
 
 # ===================getting morphologies =======================
 
@@ -114,15 +143,16 @@ out = out.join(sex, how='inner')
 
 #================ add preop native flection ======================
 # print(out.shape)
-# flexion_data = pd.read_csv(config.raw_flx_path)
+# flexion_data = pd.read_csv(config.raw_flx_path, index_col=0)
 # out = out.join(flexion_data.loc[:, 'Preop Native Flexion (Flexion=+, Extension=-)'], how='inner')
 # out.rename(axis=1, mapper={'Preop Native Flexion (Flexion=+, Extension=-)':'Preop Flexion'})
 # print(out.shape)
 """
 DONT DO THIS: 
-the flexion data unioned with the other data is so sparse that we lose 96% of the dataset
+the flexion data unioned with the other data is so sparse that we lose a lot% of the dataset
 """
 
 #================ save to csv ======================
 
 out.to_csv(config.treated_path)
+print(f"Shape: {out.shape}")
